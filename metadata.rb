514cd0d510b681d994d0bb90515c43c27eec1929
Simon McCartney <simon.mccartney@hp.com>
2013-08-22 23:14:28 +0100

    revbump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "syseng@brightcove.com"
 license          "All rights reserved"
 description      "Installs/Configures diamond"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version "1.0.17"
+version          "1.0.18"
 
 depends          'search_helper'
 depends          'repositories'

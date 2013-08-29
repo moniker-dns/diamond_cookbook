12268b2d2eeac71be1b27845f7c54f93c6308187
Kiall Mac Innes <kiall@hp.com>
2013-08-29 17:09:32 +0100

    Don't use EDB

--- recipes/mysql.rb
+++ recipes/mysql.rb
@@ -2,15 +2,12 @@
 
 include_recipe 'diamond::default'
 
-#load data bag
-mysql = Chef::EncryptedDataBagItem.load("passwords", "mysql")
-
 collector_config "MySQLCollector" do
   path    node[:diamond][:collectors][:MySQLCollector][:path]
   host    node[:diamond][:collectors][:MySQLCollector][:host]
   port    node[:diamond][:collectors][:MySQLCollector][:port]
   db      node[:diamond][:collectors][:MySQLCollector][:db]
-  user    mysql['username']
-  passwd  mysql['password']
+  user    node[:diamond][:collectors][:MySQLCollector][:username]
+  passwd  node[:diamond][:collectors][:MySQLCollector][:password]
 end
 
